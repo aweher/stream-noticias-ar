ad91083e9a78dce50bf36aeacda029d27d7f0b9d
Ariel Weher <ariel@weher.net>
2023-12-20 19:54:14 -0300

    Update Docker push commands

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 NOW=$(date +"%Y%m%d%H%M%S")
 docker build -t aweher/stream-noticias-ar:latest -t aweher/stream-noticias-ar:$NOW .
-docker push aweher/stream-noticias-ar:latest aweher/stream-noticias-ar:$NOW
+docker push aweher/stream-noticias-ar:latest 
+docker push aweher/stream-noticias-ar:$NOW
