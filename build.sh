c4174968333bbd278507ad00b03a474119a89d47
Ariel Weher <ariel@weher.net>
2024-03-15 13:46:27 -0300

    Update build.sh script to include additional Docker image tags

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-PROJECT=ghcr.io/aweher/stream-noticias-ar
+PROJECT=aweher/stream-noticias-ar
+PROJECTGH=ghcr.io/aweher/stream-noticias-ar
 NOW=$(date +"%Y%m%d%H%M%S")
-docker build -t $PROJECT:latest -t $PROJECT:$NOW .
+docker build -t $PROJECT:latest -t $PROJECT:$NOW -t $PROJECTGH:latest -t $PROJECTGH:$NOW .
 docker push $PROJECT:latest 
 docker push $PROJECT:$NOW
+docker push $PROJECTGH:latest 
+docker push $PROJECTGH:$NOW
